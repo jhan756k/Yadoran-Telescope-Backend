3606327121b933a799fcf4948cf1aaf61eb5fe94
Jooney <jhan756k@gmail.com>
2023-07-16 12:00:46 +0900

    dir test

--- models/users.js
+++ models/users.js
@@ -2,16 +2,16 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-    username: {
-        type: String,
-        required: true,
-        maxlength: 8,
-        minlength: 3,
-    },
-    password: {
-        type: String,
-        required: true,
-    },
+  username: {
+    type: String,
+    required: true,
+    maxlength: 8,
+    minlength: 3,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 });
 
 export default mongoose.model("User", userSchema);
